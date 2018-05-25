2783fc74e4b29e0713e9b053a67da4ee6622d2a6
Shaila Rahman Shammi <mayasrshammi@gmail.com>
2018-05-25 23:52:48 +0600

    Initial commit

---
+++ app/src/main/java/com/example/shammi/playyoutubevideo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.shammi.playyoutubevideo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
